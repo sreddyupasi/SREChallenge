0d21a285845369241e93ccfb51007e90afb706b5
karanbpem <42676531+karanbpem@users.noreply.github.com>
2018-08-27 10:05:11 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get install npm
 sudo pip install -U pytest
 sudo pip install selenium
 sudo pip install pytest-selenium
-sudo sudo apt-get install build-essential chrpath libssl-dev libxft-dev libfreetype6-dev libfreetype6 libfontconfig1-dev libfontconfig1 -y
+sudo apt-get install build-essential chrpath libssl-dev libxft-dev libfreetype6-dev libfreetype6 libfontconfig1-dev libfontconfig1 -y
 
 sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local/share/
